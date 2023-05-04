79e2a6f95fa9bf416d034652c3086f2959753313
Rahatul07 <rifarhad08@gmail.com>
2023-05-04 16:24:50 +0600

    food reviews section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,6 +12,19 @@ const Home = () => {
   return (
     <div>
       <Body />
+      <h1
+        className=" font-bold  text-gray-300 text-center text-5xl mb-2"
+        style={{ fontFamily: "'Parisienne', cursive" }}
+      >
+        Talent and Experience
+      </h1>
+      <h1
+        className=" font-bold  text-primary text-center text-5xl mb-10"
+        style={{ fontFamily: "'Montserrat', cursive" }}
+      >
+        Team Members
+      </h1>
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-10 md:mx-48 xl:mx-60 2xl:mx-72 gap-5 lg:gap-10  mt-20 pb-10">
         {chefs.map((chef) => (
           <Card key={chef.id} chef={chef} />
---
+++ src/pages/Reviews/Reviews.jsx
@@ -0,0 +1,242 @@
+import React from "react";
+import { AiFillStar } from "react-icons/ai";
+import img1 from "../../assets/cusines/01.png";
+
+const Reviews = () => {
+  return (
+    <>
+      <h1
+        className=" font-bold  text-primary text-center text-5xl mb-10"
+        style={{ fontFamily: "'Montserrat', cursive" }}
+      >
+        FOOD REVIEWS
+      </h1>
+      <div className="w-10/12 mx-auto grid grid-cols-1 lg:grid-cols-2">
+        <div className="card card-side glass shadow-xl h-auto mb-5">
+          <figure>
+            <img
+              className="w-36 h-auto m-5 rounded-2xl"
+              src={img1}
+              alt="Movie"
+            />
+          </figure>
+          <div className="card-body">
+            <div
+              className=" text-white text-bold text-2xl "
+              style={{ fontFamily: "'Kalam', cursive" }}
+            >
+              TIRAMISU
+            </div>
+
+            <div className=" text-white text-base">
+              Recipe by{" "}
+              <span
+                className="text-primary text-xl"
+                style={{ fontFamily: "'Parisienne', cursive" }}
+              >
+                Mario Russo
+              </span>
+            </div>
+            <div className="flex items-center">
+              <h1 className="text-gray-300 mr-2">5.0</h1>
+              <div className="flex">
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card card-side glass shadow-xl h-auto mb-5">
+          <figure>
+            <img
+              className="w-36 h-auto m-5 rounded-2xl"
+              src={img1}
+              alt="Movie"
+            />
+          </figure>
+          <div className="card-body">
+            <div
+              className=" text-white text-bold text-2xl "
+              style={{ fontFamily: "'Kalam', cursive" }}
+            >
+              TIRAMISU
+            </div>
+
+            <div className=" text-white text-base">
+              Recipe by{" "}
+              <span
+                className="text-primary text-xl"
+                style={{ fontFamily: "'Parisienne', cursive" }}
+              >
+                Mario Russo
+              </span>
+            </div>
+            <div className="flex items-center">
+              <h1 className="text-gray-300 mr-2">5.0</h1>
+              <div className="flex">
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card card-side glass shadow-xl h-auto mb-5">
+          <figure>
+            <img
+              className="w-36 h-auto m-5 rounded-2xl"
+              src={img1}
+              alt="Movie"
+            />
+          </figure>
+          <div className="card-body">
+            <div
+              className=" text-white text-bold text-2xl "
+              style={{ fontFamily: "'Kalam', cursive" }}
+            >
+              TIRAMISU
+            </div>
+
+            <div className=" text-white text-base">
+              Recipe by{" "}
+              <span
+                className="text-primary text-xl"
+                style={{ fontFamily: "'Parisienne', cursive" }}
+              >
+                Mario Russo
+              </span>
+            </div>
+            <div className="flex items-center">
+              <h1 className="text-gray-300 mr-2">5.0</h1>
+              <div className="flex">
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card card-side glass shadow-xl h-auto mb-5">
+          <figure>
+            <img
+              className="w-36 h-auto m-5 rounded-2xl"
+              src={img1}
+              alt="Movie"
+            />
+          </figure>
+          <div className="card-body">
+            <div
+              className=" text-white text-bold text-2xl "
+              style={{ fontFamily: "'Kalam', cursive" }}
+            >
+              TIRAMISU
+            </div>
+
+            <div className=" text-white text-base">
+              Recipe by{" "}
+              <span
+                className="text-primary text-xl"
+                style={{ fontFamily: "'Parisienne', cursive" }}
+              >
+                Mario Russo
+              </span>
+            </div>
+            <div className="flex items-center">
+              <h1 className="text-gray-300 mr-2">5.0</h1>
+              <div className="flex">
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card card-side glass shadow-xl h-auto mb-5">
+          <figure>
+            <img
+              className="w-36 h-auto m-5 rounded-2xl"
+              src={img1}
+              alt="Movie"
+            />
+          </figure>
+          <div className="card-body">
+            <div
+              className=" text-white text-bold text-2xl "
+              style={{ fontFamily: "'Kalam', cursive" }}
+            >
+              TIRAMISU
+            </div>
+
+            <div className=" text-white text-base">
+              Recipe by{" "}
+              <span
+                className="text-primary text-xl"
+                style={{ fontFamily: "'Parisienne', cursive" }}
+              >
+                Mario Russo
+              </span>
+            </div>
+            <div className="flex items-center">
+              <h1 className="text-gray-300 mr-2">5.0</h1>
+              <div className="flex">
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card card-side glass shadow-xl h-auto mb-5">
+          <figure>
+            <img
+              className="w-36 h-auto m-5 rounded-2xl"
+              src={img1}
+              alt="Movie"
+            />
+          </figure>
+          <div className="card-body">
+            <div
+              className=" text-white text-bold text-2xl "
+              style={{ fontFamily: "'Kalam', cursive" }}
+            >
+              TIRAMISU
+            </div>
+
+            <div className=" text-white text-base">
+              Recipe by{" "}
+              <span
+                className="text-primary text-xl"
+                style={{ fontFamily: "'Parisienne', cursive" }}
+              >
+                Mario Russo
+              </span>
+            </div>
+            <div className="flex items-center">
+              <h1 className="text-gray-300 mr-2">5.0</h1>
+              <div className="flex">
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+                <AiFillStar className="text-primary text-xl" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Reviews;
