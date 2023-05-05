189503652dcaf1ceccb71ad59e6e04447a91fee8
Rahatul07 <rifarhad08@gmail.com>
2023-05-05 16:18:19 +0600

    navbar improved

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -17,12 +17,12 @@ const Header = () => {
       });
   };
   return (
-    <div className=" mx-auto   px-10 bg-black -mb-3 shadow-lg shadow-slate-400 ">
+    <div className=" mx-auto  px-10 bg-black -mb-3 shadow-lg shadow-slate-400 ">
       <div className="navbar   text-white ">
         <div className="navbar-start">
           <img className="h-14 w-20 " src={logo} alt="" />
         </div>
-        <div className="navbar-center text-2xl uppercase h-12 pr-10 hidden lg:flex">
+        <div className="navbar-center text-xl 2xl:text-2xl uppercase h-12 pr-10 hidden lg:flex">
           <ul className="pl-3 menu-horizontal px-1">
             <li className="ml-5">
               <NavLink
@@ -62,7 +62,6 @@ const Header = () => {
               </>
             ) : (
               <>
-                {" "}
                 <li className="ml-5">
                   <NavLink
                     to="/logIn"
@@ -93,11 +92,6 @@ const Header = () => {
         </div>
 
         <div className="navbar-end ">
-          {/* <div>
-            <div className="flex items-center justify-between  mr-5 relative text-2xl">
-              
-            </div>
-          </div> */}
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
               <img title={user?.displayName} src={user?.photoURL} />
@@ -109,10 +103,10 @@ const Header = () => {
           <button
             aria-label="Open Menu"
             title="Open Menu"
-            className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+            className="p-2 -mr-1 transition text-white duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
             onClick={() => setIsMenuOpen(true)}
           >
-            <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+            <svg className="w-5 text-white" viewBox="0 0 24 24">
               <path
                 fill="currentColor"
                 d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -130,10 +124,10 @@ const Header = () => {
           {isMenuOpen && (
             <div className="absolute z-10 top-0 left-0   w-full">
               <div className="p-5  border bg-gray-700 rounded shadow-sm">
-                <div className="flex items-center justify-between mb-4">
+                <div className="flex items-center  justify-between mb-4">
                   <div>
-                    <div className="navbar-start">
-                      <img className="h-20 w-48" src={logo} alt="" />
+                    <div className="navbar-start ">
+                      <img className="h-14 w-20 ml-40" src={logo} alt="" />
                     </div>
                   </div>
                   <div>
@@ -143,10 +137,7 @@ const Header = () => {
                       className="p-2 -mt-2 -mr-2 transition text-white duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                       onClick={() => setIsMenuOpen(false)}
                     >
-                      <svg
-                        className="w-5 text-gray-600 text-white"
-                        viewBox="0 0 24 24"
-                      >
+                      <svg className="w-5  text-white" viewBox="0 0 24 24">
                         <path
                           fill="currentColor"
                           d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -156,7 +147,79 @@ const Header = () => {
                   </div>
                 </div>
                 <nav>
-                  <div className="navbar-center bg-gray-700 lg:flex">
+                  <div className="text-xl  uppercase h-12 pr-10 navbar-center bg-gray-700 text-center ">
+                    <ul className="pl-3 menu-horizontal px-1">
+                      <li className="ml-5">
+                        <NavLink
+                          to="/"
+                          aria-label="Home"
+                          title="Home"
+                          className={({ isActive }) =>
+                            isActive ? "active" : "default"
+                          }
+                        >
+                          Home
+                        </NavLink>
+                      </li>
+
+                      <li className="ml-5  ">
+                        <NavLink
+                          to="/blogs"
+                          aria-label="blogs"
+                          title="blogs"
+                          className={({ isActive }) =>
+                            isActive ? "active" : "default"
+                          }
+                        >
+                          Blogs
+                        </NavLink>
+                      </li>
+                      {user ? (
+                        <>
+                          <li className="ml-5">
+                            <NavLink
+                              aria-label="logOut"
+                              title="logOut"
+                              onClick={handleLogOut}
+                              className={({ isActive }) =>
+                                isActive ? "default" : "active"
+                              }
+                            >
+                              LogOut
+                            </NavLink>
+                          </li>
+                        </>
+                      ) : (
+                        <>
+                          <li className="ml-5">
+                            <NavLink
+                              to="/logIn"
+                              aria-label="logIn"
+                              title="logIn"
+                              className={({ isActive }) =>
+                                isActive ? "active" : "default"
+                              }
+                            >
+                              LogIn
+                            </NavLink>
+                          </li>
+                          <li className="ml-5">
+                            <NavLink
+                              to="/register"
+                              aria-label="register"
+                              title="register"
+                              className={({ isActive }) =>
+                                isActive ? "active" : "default"
+                              }
+                            >
+                              REGISTER
+                            </NavLink>
+                          </li>
+                        </>
+                      )}
+                    </ul>
+                  </div>
+                  {/* <div className="navbar-center bg-gray-700 lg:flex">
                     <ul className="pl-3  px-1">
                       <li className="ml-5 mb-3">
                         <NavLink
@@ -183,7 +246,7 @@ const Header = () => {
                           Blogs
                         </NavLink>
                       </li>
-                      <li className="ml-5">
+                      <li className="ml-5 mb-3">
                         <NavLink
                           to="/logIn"
                           aria-label="logIn"
@@ -195,7 +258,7 @@ const Header = () => {
                           LogIn
                         </NavLink>
                       </li>
-                      <li className="ml-5">
+                      <li className="ml-5 mb-3">
                         <NavLink
                           to="/register"
                           aria-label="register"
@@ -208,7 +271,7 @@ const Header = () => {
                         </NavLink>
                       </li>
 
-                      <li className="ml-5">
+                      <li className="ml-5 mb-3">
                         <NavLink
                           to="/logOut"
                           aria-label="logOut"
@@ -221,7 +284,7 @@ const Header = () => {
                         </NavLink>
                       </li>
                     </ul>
-                  </div>
+                  </div> */}
                 </nav>
               </div>
             </div>
